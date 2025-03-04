6da67b9207e233aae9b2c11c83395ced321bef61
Devon <ds.pydev@gmail.com>
2025-03-04 10:28:15 +0200

    Add get currencies and add to dic

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ load_dotenv()
 jason = {'eur': {'meta': {'last_updated_at': '2025-03-03T23:59:59Z'}, 'data': {'GBP': {'code': 'GBP', 'value': 0.8254537584}, 'MUR': {'code': 'MUR', 'value': 48.6313845297}, 'USD': {'code': 'USD', 'value': 1.0485146702}, 'ZAR': {'code': 'ZAR', 'value': 19.4908428347}}}, 'gbp': {'meta': {'last_updated_at': '2025-03-03T23:59:59Z'}, 'data': {'EUR': {'code': 'EUR', 'value': 1.2114548995}, 'GBP': {'code': 'GBP', 'value': 1}, 'MUR': {'code': 'MUR', 'value': 58.914729059}, 'USD': {'code': 'USD', 'value': 1.2702282344}, 'ZAR': {'code': 'ZAR', 'value': 23.6122770478}}}, 'mur': {'meta': {'last_updated_at': '2025-03-03T23:59:59Z'}, 'data': {'EUR': {'code': 'EUR', 'value': 0.0205628528}, 'GBP': {'code': 'GBP', 'value': 0.0169736841}, 'USD': {'code': 'USD', 'value': 0.0215604528}, 'ZAR': {'code': 'ZAR', 'value': 0.4007873315}}}, 'usd': {'meta': {'last_updated_at': '2025-03-03T23:59:59Z'}, 'data': {'EUR': {'code': 'EUR', 'value': 0.9537300988}, 'GBP': {'code': 'GBP', 'value': 0.7872600946}, 'MUR': {'code': 'MUR', 'value': 46.3812151723}, 'ZAR': {'code': 'ZAR', 'value': 18.5890034624}}}, 'zar': {'meta': {'last_updated_at': '2025-03-03T23:59:59Z'}, 'data': {'EUR': {'code': 'EUR', 'value': 0.0513061446}, 'GBP': {'code': 'GBP', 'value': 0.0423508499}, 'MUR': {'code': 'MUR', 'value': 2.4950888447}, 'USD': {'code': 'USD', 'value': 0.0537952452}}}}
 
 for x in jason:
+    name = x
     date = jason[x]['meta']['last_updated_at'][:10]
-    print(f'{x} | {date} = {jason[x]}')
+
+    print(f'{name} | {date} = {jason[x]}')
 
 # date
 # print()
